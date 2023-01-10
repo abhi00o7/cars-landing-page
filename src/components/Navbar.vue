cf72877c4b0dd5745dbc466d09496317a2bf6d2b
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2023-01-10 23:56:49 +0530

    add NavBar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,22 @@
 <template>
-  <div >
-    <h1 >Navbar</h1>
+  <div class="Nav container mx-auto my-8">
+    <div class="flex">
+      <div class="mx-4">
+        <img :src="Logo" alt="vue logo" class="w-16" />
+      </div>
+      <div class="mx-4">
+        <a href="#">Home</a>
+      </div>
+      <div class="mx-4">
+        <a href="#">About</a>
+      </div>
+      <div class="mx-4">
+        <a href="#">Contact</a>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import Logo from "../assets/logo/favicon.ico";
+</script>
